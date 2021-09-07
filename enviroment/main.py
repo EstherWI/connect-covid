865d2b9df256d46eba4f3347f83315f0b549c0b4
EstherWI <esthersss09@gmail.com>
2021-09-07 14:41:47 -0300

    update

--- enviroment/main.py
+++ enviroment/main.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+class Paciente:
+    def __init__(self, cpf, temp, freq, pressao, resp):
+        self.cpf = cpf
+        self.temp = temp
+        self.freq = freq
+        self.pressao = pressao
+        self.resp = resp
+
+p = Paciente(1,1,1,1,1)
+lista = [p]
 
 @app.route('/')
 def raiz():
@@ -10,14 +20,21 @@ def raiz():
 
 @app.route('/doctor')
 def doc():
-    return 'Ola médico'
+    return render_template('doctor.html', titulo='Pacientes', patients=lista)
 
-
-@app.route('/patient', methods=['GET', 'POST'])
+@app.route('/patient', methods=['GET', ])
 def patient():
-    if request.method == 'POST':
-        cpf = request.form.get('cpf')
     return render_template('patient.html')
 
+@app.route('/criar', methods=['POST', ])
+def criar():
+    cpf = request.form['cpf']
+    temp = request.form['temp']
+    batimentos = request.form['batimentos']
+    pressao = request.form['pressao']
+    resp = request.form['resp']
+    paciente = Paciente(cpf, temp, batimentos, pressao, resp)
+    lista.append(paciente)
+    return redirect(url_for('patient'))
 
 app.run(debug=True)
